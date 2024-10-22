d768af27c04f81c707bdb15ee95668bdec803853
Herman Tretteteig <hermat@uio.no>
2024-10-22 13:57:59 +0200

    added docker based on lxd doc

--- chap-core-lxd-container-init.sh
+++ chap-core-lxd-container-init.sh
@@ -4,10 +4,22 @@
 apt-get update -y
 apt-get upgrade -y
 
+sudo apt-get install \
+ ca-certificates \
+ curl \
+ gnupg \
+  lsb-release
+
 # Install Docker
-curl -fsSL https://get.docker.com -o get-docker.sh
-sh get-docker.sh
-rm get-docker.sh
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg \
+--dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+"deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 # Clone the chap-core repository
 git clone -b dev https://github.com/dhis2-chap/chap-core /opt/chap-core
--- deploy-chap-core.sh
+++ deploy-chap-core.sh
@@ -1,11 +1,21 @@
 
 # Check if the container exists and delete it if it does
 if sudo lxc list | grep -q "chap-container"; then
+  echo "Deleting existing container..."
   sudo lxc delete chap-container --force
 fi
 
+lxc storage create docker btrfs
 sudo lxc launch ubuntu:20.04 chap-container
+
+lxc storage volume create docker chap-container
+lxc config device add chap-container docker disk pool=docker source=chap-container path=/var/lib/docker
+lxc config set chap-container security.nesting=true security.syscalls.intercept.mknod=true security.syscalls.intercept.setxattr=true
+
+lxc restart chap-container
+
 sudo lxc config device add chap-container myport8080 proxy listen=tcp:0.0.0.0:8080 connect=tcp:127.0.0.1:8000
+
 # Wait for the container to initialize
 sleep 30 
 
