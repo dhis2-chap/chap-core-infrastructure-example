405381b743b6c90366ee9cccb69d95174ffd3094
Herman Tretteteig <hermat@uio.no>
2024-10-29 20:08:56 +0100

    Reanmed container and set environment var

--- deploy-chap-core.sh
+++ deploy-chap-core.sh
@@ -1,47 +1,42 @@
 
 # Check if the container exists and delete it if it does
-if sudo lxc list | grep -q "chap-container"; then
+if sudo lxc list | grep -q "chap-core"; then
   echo "Deleting existing container..."
-  sudo lxc delete chap-container --force
+  sudo lxc delete chap-core --force
 fi
 
-## deleting existing iptables rules
-sudo iptables -F
-
-sleep 10
 # Delete existing storage pool
-sudo lxc storage volume delete docker chap-container
+sudo lxc storage volume delete docker chap-core
 # Wait for the container to be deleted
-sleep 10
 # Delete the storage pool
 sudo lxc storage delete docker
 
-sleep 10
 
 lxc storage create docker btrfs size=100GB
-sudo lxc launch ubuntu:24.04 chap-container
+sudo lxc launch ubuntu:24.04 chap-core
 
 # Delete existing storage volume if it exists
 
 # Create new storage volume
-lxc storage volume create docker chap-container size=100GB
-lxc config device add chap-container docker disk pool=docker source=chap-container path=/var/lib/docker
-lxc config set chap-container security.nesting=true security.syscalls.intercept.mknod=true security.syscalls.intercept.setxattr=true
+lxc storage volume create docker chap-core size=100GB
+lxc config device add chap-core docker disk pool=docker source=chap-core path=/var/lib/docker
+lxc config set chap-core security.nesting=true security.syscalls.intercept.mknod=true security.syscalls.intercept.setxattr=true
 
-lxc restart chap-container
+# set environment variables
+lxc config set chap-core environment.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY $GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY
+lxc config set chap-core environment.GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY $GOOGLE_SERVICE_ACCOUNT_PRIVATE_KEY
 
-sudo lxc config device add chap-container chapPort443 proxy listen=tcp:0.0.0.0:443 connect=tcp:127.0.0.1:8000
+lxc restart chap-core
 
-# Wait for the container to initialize
-sleep 30 
+sudo lxc config device add chap-core chapPort443 proxy listen=tcp:0.0.0.0:443 connect=tcp:127.0.0.1:8000
 
 # Upload the initialization script to the container
-sudo lxc file push chap-core-lxd-container-init.sh chap-container/root/
+sudo lxc file push chap-core-lxd-container-init.sh chap-core/root/
 
 # Make the script executable
-sudo lxc exec chap-container -- chmod +x /root/chap-core-lxd-container-init.sh
+sudo lxc exec chap-core -- chmod +x /root/chap-core-lxd-container-init.sh
 
 # Run the initialization script within the container
-sudo lxc exec chap-container -- /root/chap-core-lxd-container-init.sh
+sudo lxc exec chap-core -- /root/chap-core-lxd-container-init.sh
 
 # Wait for the container to initialize
