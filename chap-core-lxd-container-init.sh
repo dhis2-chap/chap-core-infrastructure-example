bd8b430a49baebaec31357d1285574342b0a5dc7
Herman Tretteteig <hermat@uio.no>
2024-11-29 17:41:03 +0100

    added var

--- chap-core-lxd-container-init.sh
+++ chap-core-lxd-container-init.sh
@@ -11,8 +11,7 @@ sudo apt-get install \
   lsb-release
 
 # Install Docker
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg \
---dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
 "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
