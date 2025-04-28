3f93e08abea011ee0993c43485973cfb7601ee5d
Herman Tretteteig <49228830+hermantretteteig@users.noreply.github.com>
2025-04-28 10:44:29 +0200

    change to launch from dev branch

--- install-chap-core-inside-lxc.sh
+++ install-chap-core-inside-lxc.sh
@@ -24,8 +24,8 @@ echo "Running apt-get install..."
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # Clone the chap-core repository
-#git clone -b dev https://github.com/dhis2-chap/chap-core /root/chap-core
-git clone https://github.com/dhis2-chap/chap-core /root/chap-core
+git clone -b dev https://github.com/dhis2-chap/chap-core /root/chap-core
+#git clone https://github.com/dhis2-chap/chap-core /root/chap-core
 
 # Move .env file from root to chap-core directory
 mv /root/.env /root/chap-core/
