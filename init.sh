5e06098f57d3e1f4bd9ef68c3d19516c45202e26
Herman Tretteteig <hermat@uio.no>
2025-11-12 12:03:04 +0100

    run nginx install as root

--- init.sh
+++ init.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-bash ./install-start-nginx.sh || exit 1
+sudo bash ./install-start-nginx.sh || exit 1
 
 #If you are running on a server with enough space mounting a volume would not be necessary.
-bash ./mount-volume.sh
+bash ./mount-volume.sh || exit 1
 
 #Install LXD, we do this before deploying CHAP Core, since we wipe the server for every deployment
-bash ./install-lxd.sh
+bash ./install-lxd.sh || exit 1
 
-bash ./install-docker-storage.sh
+bash ./install-docker-storage.sh || exit 1
 
 #This would deploy CHAP Core
-bash ./create-lxc-container-and-install-chap-core.sh
+bash ./create-lxc-container-and-install-chap-core.sh || exit 1
